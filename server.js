10349c456721aa2f8e735b4e78c80c30e60a326a
David Mosquera <mosqueradavid@outlook.es>
2020-05-05 10:47:59 -0500

    Create coherent responses

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
-const express = require('express')
-const router = express.Router()
-const bodyParser = require('body-parser')
+const express = require("express");
+const PORT = 3000;
+const router = express.Router();
+const bodyParser = require("body-parser");
+const response = require("./network/response");
 
-var app = express()
-app.use(router)
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+var app = express();
+app.use(router);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/message', (request, response) => {
-  console.log(request.headers)
-  response.header({
-    'custom-header': 'Our custom header'
-  })
-  response.send('List of messages')
-})
+router.get("/message", (req, res) => {
+  res.header({
+    "custom-header": "Our custom header",
+  });
+  response.success(req, res, "message list");
+});
 
-app.post('/message', (request, response) => {
-  console.log(request.body)
-  console.log(request.query)
-  response.status(201).send({error: '', body: 'Successfully created' })
-})
+router.post("/message", (req, res) => {
+  console.log(req.body);
+  console.log(req.query);
+  req.query.error == "ok"
+    ? response.error(req, res, "Simulated Error: ", 400)
+    : response.success(req, res, "Successfully Created", 201);
+});
 
 // app.use('/', (request, response) => {
 //   response.send('Hola')
 // })
 
-app.listen(3000)
-console.log('App is listening on port http://localhost:3000')
+app.listen(PORT);
+console.log("App is listening on port http://localhost:3000");
