9e0a0b6864de6403326a7e5ab974046162f75c53
David Mosquera <mosqueradavid@outlook.es>
2020-05-05 07:08:41 -0500

    Receive info from a client

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const router = express.Router()
+const bodyParser = require('body-parser')
 
 var app = express()
+app.use(router)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/', (request, response) => {
-  response.send('Hola')
+app.get('/message', (request, response) => {
+  response.send('List of messages')
 })
+
+app.post('/message', (request, response) => {
+  console.log(request.body)
+  console.log(request.query)
+  response.send('Message ' + request.body.text + ' successfully added')
+})
+
+// app.use('/', (request, response) => {
+//   response.send('Hola')
+// })
 
 app.listen(3000)
 console.log('App is listening on port http://localhost:3000')
